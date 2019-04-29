0e12acf4d84c7f7c3fefac01a027867e1cefb04c
Jason Lo <github@jasonlo.email>
2019-04-28 17:30:04 -0700

    Updates

---
+++ precache-manifest.96b9444f9c9ae9afb2443071e0f6f636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befaa1b406d606a15abc3e449831e3e2",
+    "url": "/invertebrae/index.html"
+  },
+  {
+    "revision": "60826b8e88222f4e97fc",
+    "url": "/invertebrae/static/css/main.cf5db718.chunk.css"
+  },
+  {
+    "revision": "fb73b043f03557a3a11b",
+    "url": "/invertebrae/static/js/2.90d0a0bd.chunk.js"
+  },
+  {
+    "revision": "60826b8e88222f4e97fc",
+    "url": "/invertebrae/static/js/main.d443a13c.chunk.js"
+  },
+  {
+    "revision": "dd5faf35e7f54655ac52",
+    "url": "/invertebrae/static/js/runtime~main.afbf7f8b.js"
+  },
+  {
+    "revision": "473ea72ae3331ae38d649a08d10096c8",
+    "url": "/invertebrae/static/media/terminal-grotesque.473ea72a.ttf"
+  }
+]);
